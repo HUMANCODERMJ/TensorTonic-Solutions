264aece818a272a7a2a9d5119e8557007ed4abe6
Manas Jha <humancodermj@gmail.com>
2026-06-19 23:25:19 +0530

    Solve entropy-node

---
+++ entropy-node/entropy-node.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def entropy_node(y):
+    """
+    Compute entropy for a single node using stable logarithms.
+    """
+    if y == []: return 0.0
+    H = 0 # H -> entropy
+    labels, counts = np.unique(y, return_counts=True)
+    proportions = [count / len(y) for label, count in zip(labels, counts)]
+
+    print(proportions)
+
+    for proportion in proportions:
+        print(f"class label->{proportion}")
+        if proportion == 0:
+            H -= 0
+        else:
+            H -= proportion*np.log2(proportion)
+        print(f"entropy->{H} \n")
+    return H
+    
